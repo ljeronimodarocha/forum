29dae68fa0208785806411592d708355230ea39c
unknown <ljeronimodarocha@gmail.com>
2020-01-23 00:06:01 -0300

    .

---
+++ src/app/core/share/confirmPassword.validator.ts
@@ -0,0 +1,12 @@
+import { AbstractControl, Validators, FormGroup } from '@angular/forms';
+export function confirmPasswordValidator() {
+    return (group: FormGroup) => {
+        let password = group.controls['password'];
+        let confirmPassword = group.controls['replayPassword'];
+        if (!(password.value === confirmPassword.value)) {
+            return { confirmPassword: true }
+        }
+        return false;
+    }
+}
+
--- src/app/post/register/register.component.ts
+++ src/app/post/register/register.component.ts
@@ -1,3 +1,4 @@
+import { confirmPasswordValidator } from './../../core/share/confirmPassword.validator';
 import { registerService } from './register.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit, Input } from '@angular/core';
@@ -9,20 +10,19 @@ import { user } from './user';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  u: user;
   @Input() teste: FormGroup;
   constructor(private formbuilder: FormBuilder, private service: registerService) { }
-
   ngOnInit() {
     this.teste = this.formbuilder.group({
-      name: [, Validators.required],
-      email: [, [Validators.required]],
-      password: [, Validators.required],
-      replayPassword: ['', Validators.required],
-      state: []
-    });
-
-
+      name: ['', Validators.required],
+      email: ['', [Validators.required]],
+      password: ['', Validators.required],
+      replayPassword: ['', [Validators.required]],
+      state: ['', Validators.required]
+    }, { validator: confirmPasswordValidator() });
+  }
+  registerUser() {
+    this.service.save(this.teste.value);
   }
   enviar(evento) {
     if (evento) {
