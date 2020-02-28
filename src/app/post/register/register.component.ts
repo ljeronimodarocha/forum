97b1119fa4ca49d30a715fc075e729bcc23b6012
ljeronimodarocha <ljeronimodarocha@gmail.com>
2020-02-27 23:43:12 -0300

    .

--- src/app/post/register/register.component.ts
+++ src/app/post/register/register.component.ts
@@ -1,8 +1,9 @@
 import { user } from './user';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChange } from '@angular/core';
 import { debounceTime } from 'rxjs/operators';
 import { Subject } from 'rxjs';
+
 
 import { confirmPasswordValidator, checkExistsEmail } from './../../core/share/confirmPassword.validator';
 import { registerService } from './register.service';
@@ -11,26 +12,50 @@ import { registerService } from './register.service';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
-  @Input() teste: FormGroup;
-  debounce: Subject<string> = new Subject<string>();
-  constructor(private formbuilder: FormBuilder, private service: registerService) { }
-  ngOnInit() {
-    this.teste = this.formbuilder.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      replayPassword: ['', [Validators.required, Validators.minLength(6)]],
-      state: ['', Validators.required]
-    }, { validator: confirmPasswordValidator(), validators: checkExistsEmail(this.service) });
+export class RegisterComponent {
+  @Input() N: string;
+@Input() teste: FormGroup =
+  this.formbuilder.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(0)]],
+    replayPassword: ['', [Validators.required, Validators.minLength(0)]],
+    state: ['', Validators.required]
+  }, { validator: confirmPasswordValidator() });;
+debounce: Subject < string > = new Subject<string>();
+constructor(private formbuilder: FormBuilder, private service: registerService) { }
 
+
+
+
+
+registerUser(t) {
+  this.service.save(this.teste.value);
+  console.log(t);
+
+  //this.teste.reset();
+
+}
+enviar(evento) {
+  if (evento) {
+    this.teste.controls['state'].setValue(evento);
   }
-  registerUser() {
-    this.service.save(this.teste.value);
-  }
-  enviar(evento) {
-    if (evento) {
-      this.teste.controls['state'].patchValue(evento);
+}
+checkExistsEmail(string: any) {
+  console.log(string);
+
+  if (!(this.teste.controls['email'].value === '')) {
+    let u = this.service.checkEmail(this.teste.controls['email'].value);
+    u.subscribe(res => {
+      if (!(res === null)) {
+        console.log(res);
+      } else {
+        this.teste.controls['email'].setErrors(null);
+        
+        console.log(this.teste.controls['email'].hasError);
+      }
     }
+    )
   }
+}
 }
