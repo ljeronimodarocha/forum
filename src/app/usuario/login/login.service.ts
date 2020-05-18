e756dc7df38885b3839fd824dee8803b48976139
ljeronimodarocha <ljeronimodarocha@gmail.com>
2020-05-17 23:57:49 -0300

    .

--- src/app/usuario/login/login.service.ts
+++ src/app/usuario/login/login.service.ts
@@ -1,7 +1,8 @@
 import { take } from 'rxjs/operators';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
@@ -14,21 +15,14 @@ export class LoginService {
 
   }
   logar(email: string, password: string) {
-    let params = new URLSearchParams();
-    params.append('grant_type', 'password');
-
     const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Authorization': 'Basic Y2xpZW50LWlkOnNlY3JldC1pZA==',
-      }),
+      headers: new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Authorization', 'Basic Y2xpZW50LWlkOnNlY3JldC1pZA=='),
       params: new HttpParams().append('grant_type', 'password')
         .append('username', 'admin@admin.com')
         .append('password', 'admin')
-
     };
-    console.log(httpOptions);
-
-    return this.http.get(`${this.rest}`, httpOptions).pipe(take(1));
+    return this.http.post(`${this.rest}`, {withCredentials:true, httpOptions});
   }
 }
